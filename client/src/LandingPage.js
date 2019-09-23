4d428bc461e5751bf443c0c326b10c18f9560501
michaelmcguiness <michaelmcguiness123@gmail.com>
2019-09-23 13:28:29 -0400

    few more changes to landing page

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -4,28 +4,37 @@ import Logo from './images/Logo.png';
 
 // Main Landing Page Wrapper
 const Wrapper = styled.div`
-    width: 80%;
-    height: 80vh;
     display: flex;
     justify-content: space-around;
     align-items: center;
     align-self: center;
+    width: 80%;
+    height: 80vh;
 `;
 
 // Title and Button Content Wrapper
 const TitleWrapper = styled.div`
-    
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    height: 25vh;
+`;
+
+// Main Title
+const Title = styled.h1`
+    font-size: 2.0rem;
 `;
 
 export default function LandingPage() {
     return (
         <Wrapper>
             <img src={Logo} style={{height: "100%"}} alt="main"/>
-            <div>
-                <h1>Design Your Life</h1>
+            <TitleWrapper>
+                <Title>Design Your Life</Title>
                 <h3>Who you are - What you do - What you believe</h3>
                 <button>Start Now</button>
-            </div>
+            </TitleWrapper>
 
         </Wrapper>     
     )
