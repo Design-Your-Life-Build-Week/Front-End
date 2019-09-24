a22408f7c41aa92a9fc99922737da5d48c4ea2de
leslie <rleslie1015@gmail.com>
2019-09-24 09:51:26 -0500

    decided to use hooks instead of redux, using dummy data

--- client/src/components/Categories.js
+++ client/src/components/Categories.js
@@ -1,53 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import dummData
+import { data } from "../data";
 
-class Categories extends React.Component {
-    state = {
-        categories: [
-            {name: "fitness",
-            activities: [
-                {"name": "Running", dateCreated: "09212019", starRating: 5, id: 456},
-                {"name": "Swimming", dateCreated: "09212019", starRating: 5, id: 456},
-                {"name": "Zumba", dateCreated: "09302019", starRating: 5, id: 456}
-            ],
-            dateCreated: '',
-            id: 123,
-            },
-            
-        ],
-        // category: {}?
-    }
-    handleChange = (e) => {
-        this.setState({ ...this.state.categories,  [e.target.name]: e.target.value});
-        console.log(e.target.name)
-        console.log(this.state.categories)
-    }
-    
-    handleSubmit = (e, newCategory) => {
-        console.log(this.state)
+const Categories = () => {
+    // hooks
+    const [state, setState] = useState(data);
+    const [category, setCategory] = useState({})
+
+    const changeHandler = e => {
+        setCategory({...category, [e.target.name]:e.target.value});
+        console.log('checking category onChange:', category);
+    };
+
+    const submitHandler = e => {
         e.preventDefault();
-        const currentDate = Date.now();
-        newCategory = { name: this.state.categories[this.state.categories.length-1].name,  activities: [], dateCreated: currentDate, id: currentDate }
-        console.log(newCategory);
-        this.setState({categories: [...this.state.categories, newCategory]});
-        console.log("updated state", this.state)
-    }
-    render() {
-        return (
+        setState({...state, categories: [...state.categories, category]});
+        console.log('submit fired!');
+    };
 
+    return (
         <div>
             <h1>Your Categories</h1>
-          {this.state.categories.map(category => <h3 category={category} > {category.name} </h3>)}
-            <form onSubmit={this.handleSubmit}>
+    
+            <form onSubmit={submitHandler}>
+                {console.log('new state:',state)}
                 <input 
-                    name = "catergory"
-                    value = {this.state.categories.name}
-                    onChange = {this.handleChange}
-                    placeholder= "Add a Category"
-                    />
+                type="text"
+                name="name"
+                value={category.name}
+                onChange={changeHandler}
+                placeholder="Category Name"
+                />
+                <button>Add</button>
             </form>
+            
+            
+            {state.categories.map(i => {
+                return <h1 key={i.id}>{i.name}</h1>
+            })}
         </div>
         )
-    }
 }
 
-export default Categories; 
+export default Categories;
