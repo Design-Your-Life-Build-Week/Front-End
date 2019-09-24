aaa401fb14d2f3cae856b247ae2943fd6937f356
Connor R. Sullivan <crsullivan@protonmail.com>
2019-09-23 23:45:23 -0700

    Styling on Registration and Login Pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import Registration from './components/Registration'
 import './App.css';
 import LandingPage from './components/LandingPage';
 import NavBar from './components/NavBar';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <LandingPage/>
-      <Registration />
+      {/* <Login/> */}
+      {/* <Registration/> */}
     </div>
   );
 }
--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -2,11 +2,65 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 // import {axiosLoginAuth} from '../utils/axiosLoginAuth'
-
-
+import { FormBorderColor, NavColor, MainFontFamily, LoginColor, ButtonBackground, ButtonFont, ButtonHover, ButtonHoverFont } from './Styling';
 
 const RegistrationBox = styled.div `
+font-family: ${MainFontFamily};
+display:flex;
+height:100%vh;
+font-size:62.5%;
+align-items:center;
+margin:8% 12%;
+`
+const MeaningfulLife = styled.div `
+font-size:3rem;
+`
+const ColorDiv = styled.div `
+background:${NavColor};
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+`
 
+const LoginDiv = styled.div `
+display:flex;
+flex-direction:column;
+background:${LoginColor};
+border-radius:2%;
+width:40%;
+margin-left:18%;
+padding:10% 5%;
+
+`
+const Label = styled.label `
+text-align:left;
+color:white;
+font-size:1.75rem;
+`
+
+const Form = styled.form `
+width:90%;
+margin:5% 0;
+`
+
+const Input = styled.input `
+width:100%;
+height:3%;
+font-size:1.75rem;
+border: 4px solid ${FormBorderColor};
+border-radius: 4px;
+background: ${LoginColor};
+`
+
+const Button = styled.button `
+width:100%;
+font-size:1.5rem;
+background-image: ${ButtonBackground};
+color: ${ButtonFont};
+    :hover{
+        background-image: ${ButtonHover};
+        color: ${ButtonHoverFont};
+    }
+    
 `
 
 const Registration = (props) => {
@@ -46,13 +100,23 @@ const handleSubmit = event => {
 
     return (
         <RegistrationBox>
-            <h1>Create an Account</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Name" name="username" value={user.username} onChange={changeHandler}/>
-                <input type="text" name="password"  placeholder="Password" value={user.password} onChange={changeHandler}/>
+            <MeaningfulLife>
+                <h1>Have a<ColorDiv>Meaningful</ColorDiv>Life</h1>
+            </MeaningfulLife>
+            <LoginDiv> 
+                <Label>Username</Label>
+                <Form onSubmit={handleSubmit}>
+                    <Input type="text" placeholder="Username" name="username" value={user.username} onChange={changeHandler}/>
+                </Form>
+                <Label>Password</Label>
+                <Form onSubmit={handleSubmit}>
+                    <Input type="text" name="password"  placeholder="Password" value={user.password} onChange={changeHandler}/>
+                </Form>
 
-                <button type="submit">Create Account</button>
-            </form>
+                <Form onSubmit={handleSubmit}>
+                    <Button type="submit">Start Now</Button>
+                </Form>
+            </LoginDiv>       
         </RegistrationBox>
     )
     
