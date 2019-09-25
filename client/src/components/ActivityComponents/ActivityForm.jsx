b18523b3b32403b5465ef23da23119fb530324ff
Connor R. Sullivan <crsullivan@protonmail.com>
2019-09-25 11:44:41 -0700

    Acitivities styling moved over, functional on page. Now need get & pull with edit and delete as well

--- client/src/components/ActivityComponents/ActivityForm.jsx
+++ client/src/components/ActivityComponents/ActivityForm.jsx
@@ -1,8 +1,47 @@
 import React, { useState } from "react";
 import styled from 'styled-components';
+import { MainFontFamily, ButtonBackground, ButtonFont, ButtonHover, ButtonHoverFont } from '../Styling'
 
 const ActivityFormDiv = styled.div `
+font-family: ${MainFontFamily};
+font-size:62.5%;
+`
+const LabelOverInput = styled.div `
+display:flex;
+flex-direction:column;
+width:100%;
+align-items:center;
 
+`
+
+const ActivityDiv = styled.div `
+display:flex;
+`
+const Input = styled.input `
+width:20%
+font-size:1.5rem;
+`
+
+const Label = styled.label `
+font-size:1.5rem;
+`
+
+const Textarea = styled.textarea `
+width:25%;
+margin-bottom: 4%;
+font-size:1.5rem;
+
+`
+const Button = styled.button `
+font-size:1.25rem;
+border-radius:5px;
+background-image: ${ButtonBackground};
+color: ${ButtonFont};
+    :hover{
+        background-image: ${ButtonHover};
+        color: ${ButtonHoverFont};
+    }
+    
 `
 
 const ActivityForm = props => {
@@ -24,30 +63,39 @@ const ActivityForm = props => {
   return (
     <ActivityFormDiv>
         <form onSubmit={submitForm}>
-            <label htmlFor="title">Activity</label>
-            <input
-                id="activity"
-                type="text"
-                name="activity"
-                onChange={handleChanges}
-                value={activity.activity}
-            />
-            <label htmlFor="note">Description</label>
-            <textarea
-                id="description"
-                name="description"
-                onChange={handleChanges}
-                value={activity.description}
-            />
-            <label htmlFor="title">Rating 1-5, 5 being best</label>
-            <input
-                id="rating"
-                type="text"
-                name="rating"
-                onChange={handleChanges}
-                value={activity.rating}
-            />
-            <button type="submit">Add Activity</button>
+            <ActivityDiv>
+              <LabelOverInput>
+                <Label htmlFor="title">Activity</Label>
+                <Input
+                    id="activity"
+                    type="text"
+                    name="activity"
+                    onChange={handleChanges}
+                    value={activity.activity}
+                />
+                </LabelOverInput>
+                <LabelOverInput>
+                  <Label htmlFor="note">Description</Label>
+                  <Textarea
+                      id="description"
+                      name="description"
+                      onChange={handleChanges}
+                      value={activity.description}
+                  />
+                </LabelOverInput>
+                <LabelOverInput>
+                  <Label htmlFor="title">Rating 1-5, 5 being best</Label>
+                  <Input
+                      id="rating"
+                      type="text"
+                      name="rating"
+                      onChange={handleChanges}
+                      value={activity.rating}
+                  />
+                </LabelOverInput>
+              </ActivityDiv>
+              <Button type="submit">Add Activity</Button>
+
         </form>
     </ActivityFormDiv>    
   );
